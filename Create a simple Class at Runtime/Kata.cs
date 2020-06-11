bef9a8f21e08a369f8de663d853be38a184fe920
natatory <natatory@icloud.com>
2020-06-11 22:20:11 +0300

    v1

---
+++ Create a simple Class at Runtime/Kata.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_a_simple_Class_at_Runtime
+{
+    public static class Kata
+    {
+        static AssemblyBuilder assemBuilder =
+                 AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("RuntimeAssembly"),
+                 AssemblyBuilderAccess.Run);
+        static ModuleBuilder modBuilder = assemBuilder.DefineDynamicModule("DynamicModule");
+        static TypeBuilder typeBuilder;
+        public static bool DefineClass(string className, Dictionary<string, Type> properties,
+            ref Type actualType)
+        {
+            if (assemBuilder.DefinedTypes.Any(x => x.Name == className))
+            {
+                string fullName = assemBuilder.DefinedTypes.First(x => x.Name == className).FullName;
+                actualType = assemBuilder.GetType(fullName);
+                return false;
+            }
+           
+            actualType = MakeClass(className, properties);
+            return true;
+        }
+
+        private static Type MakeClass(string className, Dictionary<string, Type> properties)
+        {
+            typeBuilder = modBuilder.DefineType(className, TypeAttributes.Public);
+            foreach (KeyValuePair<string, Type> prop in properties)
+            {
+                FieldBuilder fldBldr = typeBuilder.DefineField(prop.Key,
+                                                        prop.Value,
+                                                        FieldAttributes.Private);
+
+                PropertyBuilder propBldr = typeBuilder.DefineProperty(prop.Key,
+                    PropertyAttributes.HasDefault,  prop.Value, null);
+                MethodBuilder custNameGetPropMthdBldr = typeBuilder.DefineMethod("get_" + prop.Key,
+                                       MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
+                                       prop.Value,
+                                       Type.EmptyTypes);
+                ILGenerator custNameGetIL = custNameGetPropMthdBldr.GetILGenerator();
+                custNameGetIL.Emit(OpCodes.Ldarg_0);
+                custNameGetIL.Emit(OpCodes.Ldfld, fldBldr);
+                custNameGetIL.Emit(OpCodes.Ret);
+
+                MethodBuilder custNameSetPropMthdBldr = typeBuilder.DefineMethod("set_" + prop.Key,
+                                       MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
+                                       null,
+                                       new Type[] { prop.Value });
+                ILGenerator custNameSetIL = custNameSetPropMthdBldr.GetILGenerator();
+                custNameSetIL.Emit(OpCodes.Ldarg_0);
+                custNameSetIL.Emit(OpCodes.Ldarg_1);
+                custNameSetIL.Emit(OpCodes.Stfld, fldBldr);
+                custNameSetIL.Emit(OpCodes.Ret);
+
+                propBldr.SetGetMethod(custNameGetPropMthdBldr);
+                propBldr.SetSetMethod(custNameSetPropMthdBldr);
+            }
+            return typeBuilder.CreateType();
+        }
+    }
+}
---
+++ Create a simple Class at Runtime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_a_simple_Class_at_Runtime
+{
+    public class SomeClass
+    {
+        public int SomeInt = 5;
+        public string SomeString = "str_value";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Type myType1 = typeof(object);
+            Type myType2 = typeof(object);
+
+            Dictionary<string, Type> properties;
+            SomeClass sc = new SomeClass();
+            // Define first class
+            properties = new Dictionary<string, Type> { { "SomeInt", typeof(int) }, { "SomeString", typeof(string) }, { "SomeObject", typeof(object) } };
+            Kata.DefineClass("SomeClass1", properties, ref myType1);
+            // Instantiate first class
+            var myInstance1 = Activator.CreateInstance(myType1);
+
+            properties = new Dictionary<string, Type> { { "SomeInt", typeof(int) }, { "SomeString", typeof(string) }, { "SomeObject", typeof(object) } };
+            Kata.DefineClass("SomeClass2", properties, ref myType2);
+            // Instantiate first class
+            var myInstance2 = Activator.CreateInstance(myType2);
+            if (myType1.Assembly == myType2.Assembly)
+                Console.WriteLine("SomeAssembly");
+            else Console.WriteLine("DifferentAssembly");
+
+            PropertyInfo[] propertyInfo = myType1.GetProperties();
+            foreach (PropertyInfo pi in propertyInfo)
+            {
+                Console.Write(pi.Name + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
